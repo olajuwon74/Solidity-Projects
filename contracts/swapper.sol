4626ac7b5b43ebf4d0948880dcc7cecb143717c0
Solomon <adebayosolomon74@gmail.com>
2022-03-15 20:12:28 +0100

    changes

---
+++ contracts/swapper.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "contracts/IERC20";
+import "pricefeed.sol";
+
+
+contract Swap{
+
+
+    struct Change{
+        uint amount;
+        uint reverseAmount;
+    }
+
+    
+}
