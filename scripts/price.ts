9714246bc7b5bf3a44dac85519992d3fd5458311
Solomon <adebayosolomon74@gmail.com>
2022-03-15 16:40:09 +0100

    first commit

---
+++ scripts/price.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+const { ethers } = require("ethers") // for nodejs only
+const provider = new ethers.providers.JsonRpcProvider("https://eth-kovan.alchemyapi.io/v2/KfD9WaCODckcwoaQQ8Oe2dNabwf4hkqX")
+const aggregatorV3InterfaceABI = [{ "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "description", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint80", "name": "_roundId", "type": "uint80" }], "name": "getRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "latestRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "version", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
+const addr = "0x9326BFA02ADD2366b30bacB125260Af641031331"
+const priceFeed = new ethers.Contract(addr, aggregatorV3InterfaceABI, provider)
+priceFeed.latestRoundData()
+// @ts-ignore
+    .then((roundData) => {
+        // Do something with roundData
+        console.log("Latest Round Data", ((roundData.answer) / 10**8).toFixed(2))
+    })
