fcab2ccb7489784cf5d8f7a7aa5e4a93f413765e
Solomon <adebayosolomon74@gmail.com>
2022-03-27 18:01:49 +0100

    removed structs

--- contracts/pricefeedTwo.sol
+++ contracts/pricefeedTwo.sol
@@ -21,30 +21,17 @@ contract Change{
     address internal tokenA = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
     address internal tokenB = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
 
-    
-struct Swap{
-  address _addr;
-  uint Amount_sent;
-  uint Amount_received;
-}
 
-uint public index;
-mapping (address => Swap) public swap_records;
  
  event successful(uint new_amount);
 
- function swapp(address _token1, address _token2, uint amount_sent)payable external returns(uint){
-     Swap storage myRecord = swap_records[msg.sender];
+ function swapp(address _token1, address _token2, uint amount_sent) external returns(uint){
     uint new_amount;
     require (amount_sent > 0, "please increase the amount you are trying to send");
     IERC20(_token1).transferFrom(msg.sender, address(this), amount_sent);
     new_amount = rev_swapp(amount_sent);
     IERC20(_token2).transfer(msg.sender, new_amount);
     emit successful(new_amount);
-    myRecord._addr = msg.sender;
-    myRecord.Amount_sent = amount_sent;
-    myRecord.Amount_received = new_amount;
-    index++;
     return new_amount;
 }
 
